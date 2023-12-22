10591e903d4961ec8c5bcb58e27a1047a7e0a93e
Jonathan Abbett <jabbett@sharpen.com>
2023-12-22 10:28:37 -0500

    Update: New high risk progress image, fixes #20 Update: sharpen-progress-dial takes in free-form description attribute, fixes #19

--- src/components/sharpen-progress-dial/sharpen-progress-dial.stories.ts
+++ src/components/sharpen-progress-dial/sharpen-progress-dial.stories.ts
@@ -25,23 +25,19 @@ export default {
       description: 'The current progress value',
       table: { defaultValue: { summary: 0 } }
     },
-    showDescription: {
-      description: 'Whether or not to display the status description'
-    },
-    statusLabel: {
-      description: 'An optional label to append after the computed threshold name'
+    description: {
+      description: 'Optional description to show below the dial'
     }
   },
   render: (args) => `
-<sharpen-progress-dial progress-val="${args.progressVal}"${optional('show-description', args.showDescription)}${optional('status-label', args.statusLabel)}${optional('high-threshold', args.highThreshold)}${optional('low-threshold', args.lowThreshold)}${optional('elevated-threshold', args.elevatedThreshold)}></sharpen-progress-bar>`
+<sharpen-progress-dial progress-val="${args.progressVal}"${optional('description', args.description)}${optional('high-threshold', args.highThreshold)}${optional('low-threshold', args.lowThreshold)}${optional('elevated-threshold', args.elevatedThreshold)}></sharpen-progress-bar>`
 };
 
 /** Including custom thresholds. */
 export const AllOptions = {
   args: {
     progressVal: 170,
-    showDescription: true,
-    statusLabel: 'Risk',
+    description: "Moderate Risk",
     highThreshold: 200,
     lowThreshold: 75,
     elevatedThreshold: 180
@@ -51,25 +47,17 @@ export const AllOptions = {
 export const ThresholdColors = {
   render: () => `
 <div class="row">
-  <div class="col-sm"><sharpen-progress-dial progress-val="0" show-description="true"></sharpen-progress-dial></div>
-  <div class="col-sm"><sharpen-progress-dial progress-val="10" show-description="true"></sharpen-progress-dial></div>
-  <div class="col-sm"><sharpen-progress-dial progress-val="50" show-description="true"></sharpen-progress-dial></div>
-  <div class="col-sm"><sharpen-progress-dial progress-val="90" show-description="true"></sharpen-progress-dial></div>
+  <div class="col-sm"><sharpen-progress-dial progress-val="0"  description="Result Pending"></sharpen-progress-dial></div>
+  <div class="col-sm"><sharpen-progress-dial progress-val="10" description="Low Risk"></sharpen-progress-dial></div>
+  <div class="col-sm"><sharpen-progress-dial progress-val="50" description="Moderate Risk"></sharpen-progress-dial></div>
+  <div class="col-sm"><sharpen-progress-dial progress-val="90" description="High Risk"></sharpen-progress-dial></div>
 </div>
 `
 };
 
-export const WithDescriptionAndLabel = {
-  args: {
-    showDescription: true,
-    statusLabel: 'Risk',
-    progressVal: 50
-  }
-};
-
 export const WithDescription = {
   args: {
-    showDescription: true,
+    description: "Moderate Risk",
     progressVal: 50
   }
 };
